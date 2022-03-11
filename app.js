8496658b9cad0036b74d8ad3bbce1c16aaf8a4a5
MarioDuval <77450981+MarioDuval@users.noreply.github.com>
2022-03-11 20:31:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use((req,res,next)=>{
 app.use('/movies',moviesRouter);
 //Otros direccionaminetos ...
 
+app.use('/users', usersRouter);
 
 app.use('*',errorRouter);
 app.use(clientErrorHandler);
