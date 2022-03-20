a0d88b648ee156522906848323e71b5837a0d217
MarioDuval <77450981+MarioDuval@users.noreply.github.com>
2022-03-20 14:09:06 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,41 @@
-// import userModel from '../models/userModel.js';
+import userModel from '../models/userModel.js';
 import bcrypt from 'bcrypt';
+import HttpError from 'http-errors';
+
 
 const register = (req, res, next) => {
+    console.log('---> userController::register');
+    try {
+        let users = userModel.registerUser(req.body);
+        if (users != undefined) {
+            next(HttpError(400, {message: 'UPS!! Usuario Existente'}));
+        } else {
+            users = userModel.registerUser(req.body);
+    
+            res.json(users);
+        }
+    } catch (error) {
+        next(error)
+    }
 
-    const user = {username: req.body.username, password: req.body.password};
-
-    res.json(user);
-
-}
+};
 
 const login = (req, res, next) => {
-    const password = {username: req.body.username, password: req.body.password};
-    
-    res.json(password)
+    console.log('---> userController::register');
+    try {
+        const users = userModel.loginUser(req.body);
+    if (users === undefined){
+        next(HttpError(400, {message: 'Usuario o contraseña incorrecto!'}));
+    } else {
+        if (bcrypt.compareSync(req.body.password, users.password))
 
-}
+        res.json(users);
+    }
+    } catch (error) {
+        next(error)
+    }
+
+};
 
 export default {
     register,
